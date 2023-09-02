864fdd559b1f3dd89f05fc0e839bf0932f187fea
wamoonsky7414 <wenjieding414@gmail.com>
2023-09-02 19:43:29 +0800

    clear separate
    still need to make function work and understand what is the different between class, self, iteratioin......

--- ACE2023_v3/geneticalgorithm.py
+++ ACE2023_v3/geneticalgorithm.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
 import ace_lib as ace
+import helpful_functions as hf
 import random
 import math
+import matplotlib.pyplot as plt
 from tqdm.notebook import tqdm
 import warnings
 import numpy as np
 np.set_printoptions(suppress=True)
-warnings.filterwarnings("ignore")
+# warnings.filterwarnings("ignore")
 
 class GeneticAlgorithm():
-    def __init__(self, father_num=200, gene_num=3,
-                       son_num=4, crossoverrate=0.9, 
-                       mutationrate=0.1, maxiteration=20 ):
+    def __init__(self, father_num=30, gene_num=3,
+                       son_num=10, crossoverrate=0.9, 
+                       mutationrate=0.1, maxiteration=5 ):
         self.F = father_num
         self.G = gene_num
         self.S = son_num
@@ -31,12 +33,12 @@ class GeneticAlgorithm():
                     expression = f"{m}( {x}, {y} )"
                     father_expression_list.append(expression)
         if len(father_expression_list) <= self.F:
-            random.sample(father_expression_list, self.F)
+            random.sample(father_expression_list, self.F)  #deap
         else:
             return father_expression_list
         return father_expression_list
     
-    def alphagenerate(generation_expression_list):
+    def alphagenerate(generation_expression_list, df):
         Father_alpha_list = [ace.generate_alpha( x, region="USA", universe="TOP3000", neutralization="SUBINDUSTRY",) for x in generation_expression_list]
         return Father_alpha_list
 
@@ -45,11 +47,11 @@ class GeneticAlgorithm():
         df.to_csv(save_path, index=False)
         print(rf'{df} already save in : {save_path}')
     
-    def fitnessfilter(df, turnoverlimite = 1.1, fitnessrank = 10):
+    def fitnessfilter(df, turnoverlimite = 0.8, fitnessrank = 10):
         data = df
         turnoverlimite = turnoverlimite
         fitnessrank = fitnessrank 
-        filtered_data = data[data['turnover'] < turnoverlimite] # select the suitable turnover "1.1"
+        filtered_data = data[data['turnover'] < turnoverlimite] # select the suitable turnover "0.7"
         survive_fitness_data = filtered_data.nlargest( fitnessrank , 'fitness')  # select the greatest alpha "10"
         return survive_fitness_data
 
@@ -91,68 +93,56 @@ class GeneticAlgorithm():
                 nhmutedgene = expression_list[i] # nh means nothing happen
                 new_generation_expression_list.append(nhmutedgene)
 
-    #to do 
-    #make a loop for main()
-def main():
-    ga = GeneticAlgorithm()
+    def main(self, df):
+        print('father_num :', self.F, 
+              'gene_num :', self.G, 
+              'son_num :', self.S,
+              'crossoverrate :', self.cr,
+              'mutationrate :', self.mr,
+              'maxiteration :', self.max_iter)
+        it = 0
+        while it < self.max_iter: # where the loop start
+            father_expression_list = self.generatefather(df ,num_sampindatafield = 200, num_ingeneration = 200)
+            if it >= 1:
+                father_expression_list.append(newgeneration_list)
+            else:
+                pass
+            father_alpha_df_list = self.alphagenerate(father_expression_list)
+            father_alpha_df = pd.DataFrame(father_alpha_df_list)
+            father_alpha_df_csv= self.datasafe(father_alpha_df)
+            result = ace.simulate_alpha_list_multi(s, father_alpha_df)
+            len(result)
+            result[0].keys()
+            result[0]['is_stats']
+            result_performance = hf.prettify_result(result, detailed_tests_view=False)
+            result_performance.head()
+            result_performance_csv = self.datasafe(result_performance) # show and safe the simulation result
+        
+            survive_fitness_data = self.fitnessfilter(result_performance) # fitnessfuntion
+            survive_fitness_data_csv = self.datasafe(survive_fitness_data)
 
-    print('father_num :', self.F, #need to make those 'self.X' become str
-          'gene_num :', self.G, 
-          'son_num :', self.S
-          'crossoverrate :', self.cr
-          'mutationrate :', self.mr
-          'maxiteration :', self.max_iter)
-    it = 0
-    while it < ga.max_iter:# where the loop start
-        father_expression_list = ga.generatefather(df = combined_df ,num_sampindatafield = 200, num_ingeneration = 200)
-    if it >= 1:
-        merge(father_expression_list, newgeneration_list) # the one thing ledt of the all code of GA
-    else:
-        return father_expression_list
-    father_alpha_df_list = ga.alphagenerate(father_expression_list)
-    father_alpha_df = pd.DataFrame(father_alpha_df_list)
-    father_alpha_df_csv= ga.datasafe(father_alpha_df)
+            best_fitness_iteration = [] # iterate graphic
+            best_fitness_iteration.append(survive_fitness_data['fitness'][0]) 
+
+            son_expression_list = self.crossover(df,crossoverrate = 0.9) # crossover
+        
+            mutagene_list = self.generatefather(df, num_sampindatafield = 1, num_ingeneration = len(son_expression_list) ) # It's wierd because this should be at ga.mutation
+            newgeneration_list = self.mutation(son_expression_list, mutagene_list, mutationrate=0.1)
+            newgeneration_list_csv = self.datasafe(newgeneration_list)
+            
+            it += 1
     
-    result = ace.simulate_alpha_list_multi(s, father_alpha_df)
-    len(result)
-    result[0].keys()
-    result[0]['is_stats']
-    result_performance = hf.prettify_result(result, detailed_tests_view=False)
-    result_performance.head()
-    result_performance_csv = ga.datasafe(result_performance) # show and safe the simulation result
+        print('The best 10 fitness: ', min(survive_fitness_data))
+        best_index = survive_fitness_data.index(max(fitness))
+        print('Setup list is: ')
+        print(survive_fitness_data[best_index])
+        plt.figure(figsize = (15,8))
+        plt.xlabel("Iteration",fontsize = 15)
+        plt.ylabel("Fitness",fontsize = 15)
+        plt.plot(best_fitness_iteration,linewidth = 2, label = "Best fitness convergence", color = 'b')
+        plt.legend()
+        plt.show()
     
-    survive_fitness_data = ga.fitnessfilter(result_performance) # fitnessfuntion
-    survive_fitness_data_csv = ga.datasafe(survive_fitness_data)
-    son_expression_list = ga.crossover(df,crossoverrate = 0.9) # crossover
-
-    mutagene_list = ga.generatefather(df = combined_df ,num_sampindatafield = 1, num_ingeneration = len(son_expression_list) ) # It's wierd because this should be at ga.mutation
-    newgeneration_list = ga.mutation(son_expression_list, mutagene_list, mutationrate=0.1)
-    newgeneration_list_csv = ga.datasafe(newgeneration_list)
-    
-    
-    #Store best result
-    every_best_value = []
-    every_best_value.append(best_valuelist[0])
-    for i in range(ga.max_iter-1):
-        if every_best_value[i] >= best_valuelist[i+1]:
-            every_best_value.append(best_valuelist[i+1])
-
-        elif every_best_value[i] <= best_valuelist[i+1]:
-            every_best_value.append(every_best_value[i])
-
-    print('The best fitness: ', min(best_valuelist))
-    best_index = best_valuelist.index(min(best_valuelist))
-    print('Setup list is: ')
-    print(best_rvlist[best_index])
-
-    plt.figure(figsize = (15,8))
-    plt.xlabel("Iteration",fontsize = 15)
-    plt.ylabel("Fitness",fontsize = 15)
-
-    plt.plot(every_best_value,linewidth = 2, label = "Best fitness convergence", color = 'b')
-    plt.legend()
-    plt.show()
-    
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main() 
 
